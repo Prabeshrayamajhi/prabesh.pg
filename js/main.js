5cf625c2cc66d94d667303530678466f6e8288a0
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-07-30 22:44:17 +0545

    nav-links modified

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const Links = document.querySelector(".header .links");
+const Link = document.querySelectorAll(".header .links .link");
 const toggler = document.querySelector(".fa-bars");
 
-console.log(toggler);
+console.log(Link);
 
 toggler.addEventListener("click", () => {
   Links.classList.toggle("show");
 });
+
+let whenClicked = () => {
+  Links.classList.remove("show");
+};
